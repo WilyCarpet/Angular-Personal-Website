f3a381da1e4cf8b24be1e845d6562da7de993b49
WilyCarpet <126929634+WilyCarpet@users.noreply.github.com>
2025-01-05 13:59:08 -0600

    Changed Home Page
    Made changes to the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home-page/home-page.component';
 import { AboutPageComponent } from './features/about-page/about-page.component';
+import { ProjectsPageComponent } from './features/projects-page/projects-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'about',component: AboutPageComponent},
+    { path: 'projects', component: ProjectsPageComponent},
     { path: '**', redirectTo: '' },
 ];
