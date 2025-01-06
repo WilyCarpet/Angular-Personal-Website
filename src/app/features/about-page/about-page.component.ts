d6aaada74f6c3907ea67b5ba271c2e4bcdc0bad2
WilyCarpet <126929634+WilyCarpet@users.noreply.github.com>
2025-01-06 13:53:43 -0600

    Updated about me page
    about me page has more depth to it

--- src/app/features/about-page/about-page.component.ts
+++ src/app/features/about-page/about-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [],
+  imports: [    MatCardModule, MatGridListModule,],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
